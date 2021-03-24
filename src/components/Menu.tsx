56e622ba018d94baaac2de3cc18b6dcc3bf4e5ab
sidearrow <sidearrow1219@gmail.com>
2021-03-24 18:33:24 +0900

    fix deploy flow

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 
 const menuItems = [
   {
@@ -29,28 +28,13 @@ const menuItems = [
   },
 ];
 
-type MenuItemProps = {
-  isActive: boolean;
-};
-
-const MenuItem: React.FC<MenuItemProps> = ({ children }) => {
-  return (
-    <span className="px-2 py-1 inline-block bg-gray-100 rounded m-1">
-      {children}
-    </span>
-  );
-};
-
 export const Menu: React.FC = () => {
-  const router = useRouter();
-  const pathname = router.pathname;
-
   return (
     <div className="pb-4 text-center">
       {menuItems.map((item, i) => (
         <Link href={item.path} key={i}>
-          <a>
-            <MenuItem isActive={item.path === pathname}>{item.text}</MenuItem>
+          <a className="px-2 py-1 inline-block bg-gray-100 rounded m-1">
+            {item.text}
           </a>
         </Link>
       ))}
